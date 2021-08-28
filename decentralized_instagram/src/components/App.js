96448c4f5049a7f93f56abc79fa7526b9e9080f4
jasonshi10 <jasonxinshi@pm.me>
2021-08-27 17:44:26 -0700

    decentralized instagram, added 3 more functions to app.js

--- decentralized_instagram/src/components/App.js
+++ decentralized_instagram/src/components/App.js
@@ -3,17 +3,122 @@ import Web3 from 'web3';
 import Identicon from 'identicon.js';
 import './App.css';
 import Decentragram from '../abis/Decentragram.json'
+// import Component from Navbar.js
 import Navbar from './Navbar'
 import Main from './Main'
 
+// declare IPFS
+const ipfsClient = require('ipfs-http-client')
+const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https'})
 
 class App extends Component {
+  // this part is reusable, load web3.js
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  // load blockchain data and connect to MetaMask
+  async loadBlockchainData() {
+    const web3 = window.web3
+    // load account
+    const accounts = await web3.eth.getAccounts()
+    // create a state and save the account there
+    this.setState({ account: accounts[0] })
+
+    // get NetworkID to identify the network that smart contract deployed
+    const networkId = await web3.eth.net.getId()
+    const networkData = Decentragram.networks[networkId]
+
+    // create js version of smart contract using web3, abi, and address
+    if(networkData) {
+      const decentragram = web3.eth.Contract(Decentragram.abi, networkData.address)
+      this.setState({ decentragram })
+      const imagesCount = await decentragram.methods.imageCount().call()
+      this.setState({ imagesCount })
+
+      // iterate through uploaded images and load them
+      for (var i = 1; i <= imagesCount; i++) {
+        const image = await decentragram.methods.images(i).call()
+        this.setState({
+          images: [...this.state.images, image]
+        })
+      }
+
+      // sort images, highest tipped on top
+      this.setState({
+        images: this.state.images.sort((a,b) => b.tipAmount - a.tipAmount)
+      })
+
+      this.setState({ loading: false})
+    } else {
+      window.alert('Decentragram contract not deployed to the detected network.')
+    }
+  }
+
+  captureFile = event => {
+  event.preventDefault()
+  const file = event.target.files[0]
+  const reader = new window.FileReader()
+  //convert to buffer file to IPFS
+  reader.readAsArrayBuffer(file)
+
+  reader.onloadend = () => {
+    this.setState({ buffer: Buffer(reader.result) })
+    console.log('buffer', this.state.buffer)
+    }
+  }
+
+  uploadImage = description => {
+    console.log("Submitting file to ipfs...")
+
+    //adding file to the IPFS
+    ipfs.add(this.state.buffer, (error, result) => {
+      console.log('Ipfs result', result)
+      if(error) {
+        console.error(error)
+        return
+      }
+
+      this.setState({ loading: true })
+      this.state.decentragram.methods.uploadImage(result[0].hash, description).send({ from: this.state.account }).on('transactionHash', (hash) => {
+        this.setState({ loading: false })
+      })
+    })
+  }
+
+  tipImageOwner(id, tipAmount) {
+    this.setState({ loading: true })
+    this.state.decentragram.methods.tipImageOwner(id).send({ from: this.state.account, value: tipAmount }).on('transactionHash', (hash) => {
+      this.setState({ loading: false })
+    })
+  }
 
   constructor(props) {
     super(props)
     this.state = {
       account: '',
+      decentragram: null,
+      images: [],
+      loading: true
     }
+
+    this.uploadImage = this.uploadImage.bind(this)
+    this.tipImageOwner = this.tipImageOwner.bind(this)
+    this.captureFile = this.captureFile.bind(this)
   }
 
   render() {
@@ -23,7 +128,11 @@ class App extends Component {
         { this.state.loading
           ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
           : <Main
-            // Code...
+              // load functions so that we can pass them to Main.js and can call them
+              images = {this.state.images}
+              captureFile = {this.captureFile}
+              uploadImage = {this.uploadImage}
+              tipImageOwner={this.tipImageOwner}
             />
           }
         }
