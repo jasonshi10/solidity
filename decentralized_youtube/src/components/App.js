17b77f1d3891c0a2a22143cfd6c7f6c14adfcf74
jasonshi10 <jasonxinshi@pm.me>
2021-09-07 19:06:41 -0700

    add upload video functionality

--- decentralized_youtube/src/components/App.js
+++ decentralized_youtube/src/components/App.js
@@ -5,12 +5,12 @@ import Main from './Main'
 import Web3 from 'web3';
 import './App.css';
 
-//Declare IPFS
+//Connect to IPFS
 const ipfsClient = require('ipfs-http-client')
 const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' }) // leaving out the arguments will default to these values
 
 class App extends Component {
-
+  // lifecycle call back in react to call loadweb3 and loadBlockchainData
   async componentWillMount() {
     await this.loadWeb3()
     await this.loadBlockchainData()
@@ -32,61 +32,120 @@ class App extends Component {
   async loadBlockchainData() {
     const web3 = window.web3
     //Load accounts
-    //Add first account the the state
+    const accounts = await web3.eth.getAccounts()
+
+    //Add first account to the the state
+    this.setState({ account: accounts[0] })
 
     //Get network ID
+    const networkID = await web3.eth.net.getId()
+
     //Get network data
-    //Check if net data exists, then
+    const networkData = DVideo.networks[networkID]
+
+    //Check if net data exists, then load the contract
+    if (networkData) {
       //Assign dvideo contract to a variable
+      const dvideo = new web3.eth.Contract(DVideo.abi, networkData.address)
       //Add dvideo to the state
+      this.setState({ dvideo })
+      //Check videosCount
+      const videosCount = await dvideo.methods.videoCount().call()
+      //Add videosCount to the state
+      this.setState({ videosCount })
 
-      //Check videoAmounts
-      //Add videAmounts to the state
-
-      //Iterate throught videos and add them to the state (by newest)
-
-
-      //Set latest video and it's title to view as default 
+      // load videos, sort by newest
+      // iterate throught videos and add them to the state (by newest)
+      for (var i=videosCount; i>=1; i--) {
+        const video = await dvideo.methods.videos(i).call()
+        this.setState({
+          videos: [...this.state.videos, video]
+        })
+      }
+      const latest = await dvideo.methods.videos(videosCount).call()
+      //Set latest video and it's title to view as default
+      this.setState({
+        currentHash: latest.hash,
+        currentTitle: latest.title
+      })
       //Set loading state to false
-
-      //If network data doesn't exisits, log error
+      this.setState({ loading: false})
+    } else { //If network data doesn't exisits, log error
+      window.alert('DVideo contract is not deployed to detected network.')
+    }
   }
 
   //Get video
   captureFile = event => {
+    event.preventDefault()
+    const file = event.target.files[0]
+    const reader = new window.FileReader()
+    reader.readAsArrayBuffer(file)
 
+    reader.onloadend = () => {
+      this.setState({ buffer: Buffer(reader.result) })
+      console.log('buffer', this.state.buffer)
+    }
   }
 
   //Upload video
   uploadVideo = title => {
+    console.log('Submitting file to IPFS...')
+
+    // add file to IPFS
+    ipfs.add(this.state.buffer, (error, result) => {
+      console.log('IPFS result', result)
+      if(error) {
+        console.error(error)
+        return
+      }
+      this.setState({ loading: true})
+      this.state.dvideo.methods.uploadVideo(result[0].hash, title).send({ from: this.state.account }).on('transactionHash', (hash) => {this.setState({ loading: false})
+      })
+    })
 
   }
 
   //Change Video
   changeVideo = (hash, title) => {
-
+    this.setState({'currentHash': hash});
+    this.setState({'currentTitle': title});
   }
 
   constructor(props) {
     super(props)
     this.state = {
-      loading: false
-      //set states
+      buffer: null,
+      account: '',
+      dvideo: null,
+      videos: [],
+      loading: true,
+      currentHash: null,
+      currentTitle: null
     }
 
-    //Bind functions
+    this.uploadVideo = this.uploadVideo.bind(this)
+    this.captureFile = this.captureFile.bind(this)
+    this.changeVideo = this.changeVideo.bind(this)
   }
 
   render() {
     return (
       <div>
-        <Navbar 
-          //Account
+        <Navbar
+          //fetch account
+          account = {this.state.account}
         />
         { this.state.loading
           ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
           : <Main
               //states&functions
+              videos={this.state.videos}
+              uploadVideo={this.uploadVideo}
+              captureFile={this.captureFile}
+              changeVideo={this.changeVideo}
+              currentHash={this.state.currentHash}
+              currentTitle={this.state.currentTitle}
             />
         }
       </div>
--- decentralized_youtube/src/components/Main.js
+++ decentralized_youtube/src/components/Main.js
@@ -5,40 +5,58 @@ class Main extends Component {
   render() {
     return (
       <div className="container-fluid text-monospace">
-      <br></br>
-      &nbsp;
-      <br></br>
-        <div className="row">
-          <div className="col-md-10">
-            <div className="embed-responsive embed-responsive-16by9" style={{ maxHeight: '768px'}}>
-              {/* Video... */}
-            </div>
-            <h3>{/* Code... */}</h3>
+          <br></br>
+          &nbsp;
+          <br></br>
+          <div className="row">
+            <div className="col-md-10">
+              <div className="embed-responsive embed-responsive-16by9" style={{ maxHeight: '768px'}}>
+                <video
+                  src={`https://ipfs.infura.io/ipfs/${this.props.currentHash}`}
+                  controls
+                >
+                </video>
+              </div>
+            <h3><b><i>{this.props.currentTitle}</i></b></h3>
           </div>
-          <div className="col-md-2 overflow-auto text-center" style={{ maxHeight: '768px', minWidth: '175px' }}>
-            <h5><b>{/* Share Video */}</b></h5>
+          <div className="col-md-2 border border-danger overflow-auto text-center" style={{ maxHeight: '768px', minWidth: '175px' }}>
+            <h5><b>Share Video</b></h5>
             <form onSubmit={(event) => {
-              {/* Upload Video...*/}
+              event.preventDefault()
+              const title = this.videoTitle.value
+              this.props.uploadVideo(title)
             }} >
               &nbsp;
-              {/* Get Video...*/}
-              <div className="form-group mr-sm-2">
-                {/* Input...*/}
-              </div>
-              {/* Button...*/}
+              <input type='file' accept=".mp4, .mkv .ogg .wmv" onChange={this.props.captureFile} style={{ width: '250px' }} />
+                <div className="form-group mr-sm-2">
+                  <input
+                    id="videoTitle"
+                    type="text"
+                    ref={(input) => { this.videoTitle = input }}
+                    className="form-control-sm"
+                    placeholder="Title..."
+                    required />
+                </div>
+              <button type="submit" className="btn btn-danger btn-block btn-sm">Upload!</button>
               &nbsp;
             </form>
-            {/* Map Video...*/}
-              {/* Return Video...*/}
-              <div style={{ width: '175px'}}>
-                <div className="card-title bg-dark">
-                  <small className="text-white"><b>{/*Video title*/}</b></small>
-                </div>
-                  <div>
-                    {/* Change Video...*/}
-                    {/* Return Side Videos...*/}
+            { this.props.videos.map((video, key) => {
+              return(
+                <div className="card mb-4 text-center bg-secondary mx-auto" style={{ width: '175px'}} key={key} >
+                  <div className="card-title bg-dark">
+                    <small className="text-white"><b>{video.title}</b></small>
                   </div>
-              </div>
+                  <div>
+                    <p onClick={() => this.props.changeVideo(video.hash, video.title)}>
+                      <video
+                        src={`https://ipfs.infura.io/ipfs/${video.hash}`}
+                        style={{ width: '150px' }}
+                      />
+                    </p>
+                  </div>
+                </div>
+              )
+            })}
           </div>
         </div>
       </div>
