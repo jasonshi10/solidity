f3f438676ed80964c75aa5ee3f5adc390ec51b02
jasonshi10 <jasonxinshi@pm.me>
2022-01-14 13:42:50 -0800

    add application section

---
+++ solidity_by_exapmle_application.sol
@@ -0,0 +1,408 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+/*
+Ether Wallet
+an example of a basic wallet.
+- anyone can send ETH.
+- only the owner can withdraw.
+*/
+
+contract EtherWallet {
+    address payable public owner;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    receive() external payable {}
+
+    function withdraw(uint _amount) external {
+        require(msg.sender == owner, "caller is not owner");
+        payable(msg.sender).transfer(_amount);
+    }
+
+    function getBalance() external view returns (uint) {
+        return address(this).balance;
+    }
+}
+
+/*
+Multi-sig wallet
+let's create an muti-sig wallet. here are the specifications.
+the wallet owners can
+- submit a transaction
+- approve and revoke approval of pending transactions
+- anyone can execute a transaction after enough owners has approved it.
+
+In contrast to standard cryptocurrency wallets, multisig wallets allow multiple individuals to sign a single transaction.
+This setup is particularly useful in situations where a large amount of funds is stored or owned by a group or corporate entity.
+
+For example, a crypto firm offering custodian services would use multisig wallets to prevent employees from going rogue or to simply add an additional layer of protection in case of a hack.
+Whether the action is malicious or not, multisig wallets make it harder for users to transfer funds as the wallet requires a signature from all or a majority of its owners - depending on the configuration.
+*/
+contract MultiSigWallet {
+    // Event is an inheritable member of a contract. An event is emitted, it stores the arguments passed in transaction logs
+    event Deposit(address indexed sender, uint amount, uint balance);
+    event SubmitTransaction(
+        address indexed owner,
+        uint indexed txIndex,
+        address indexed to,
+        uint value,
+        bytes data
+    );
+    event ConfirmTransaction(address indexed owner, uint indexed txIndex);
+    event RevokeConfirmation(address indexed owner, uint indexed txIndex);
+    event ExecuteTransaction(address indexed owner, uint indexed txIndex);
+
+    address[] public owners;
+    mapping(address => bool) public isOwner;
+    uint public numConfirmationsRequired;
+
+    // structs are a way to define new custom types in Solidity
+    struct Transaction {
+        address to;
+        uint value;
+        bytes data;
+        bool executed;
+        uint numConfirmations;
+    }
+
+    // mapping from tx index => owner => bool
+    mapping(uint => mapping(address => bool)) public isConfirmed;
+
+    Transaction[] public transactions;
+
+    // Modifiers are used to modify the behaviour of a function
+    // The function body is inserted where the special symbol "_;" appears in the definition of a modifier.
+    // So if condition of modifier is satisfied while calling this function, the function is executed and otherwise, an exception is thrown.
+    modifier onlyOwner() {
+        require(isOwner[msg.sender], "not owner");
+        _;
+    }
+
+    modifier txExists(uint _txIndex) {
+        require(_txIndex < transactions.length, "tx does not exist");
+        _;
+    }
+
+    modifier notExecuted(uint _txIndex) {
+        require(!transactions[_txIndex].executed, "tx already executed");
+        _;
+    }
+
+    modifier notConfirmed(uint _txIndex) {
+        require(!isConfirmed[_txIndex][msg.sender], "tx already confirmed");
+        _;
+    }
+
+    // constructor invokes only once when the contract is deployed and is used to initialize the contract state
+    constructor(address[] memory _owners, uint _numConfirmationsRequired) {
+        require(_owners.length > 0, "owners required");
+        require(
+            _numConfirmationsRequired > 0 &&
+                _numConfirmationsRequired <= _owners.length,
+                "invalid number of required confirmations"
+        );
+
+        for (uint i = 0; i < _owners.length; i++) {
+            address owner = _owners[i];
+
+            require(owner != address(0), "invalid owner");
+            require(!isOwner[owner], "owner not unique");
+
+            isOwner[owner] = true;
+            owners.push(owner);
+        }
+        numConfirmationsRequired = _numConfirmationsRequired;
+    }
+
+    receive() external payable {
+        emit Deposit(msg.sender, msg.value, address(this).balance);
+    }
+
+    function submitTransaction(
+        address _to,
+        uint _value,
+        bytes memory _data
+    ) public onlyOwner {
+        uint txIndex = transactions.length;
+
+        transactions.push(
+            Transaction({
+                to: _to,
+                value: _value,
+                data: _data,
+                executed: false,
+                numConfirmations: 0
+            })
+        );
+        emit SubmitTransaction(msg.sender, txIndex, _to, _value, _data);
+    }
+
+    function confirmTransaction(uint _txIndex)
+        public
+        onlyOwner
+        txExists(_txIndex)
+        notExecuted(_txIndex)
+        notConfirmed(_txIndex)
+        {
+            Transaction storage transaction = transactions[_txIndex];
+            transaction.numConfirmations += 1;
+            isConfirmed[_txIndex][msg.sender] = true;
+
+            emit ConfirmTransaction(msg.sender, _txIndex);
+        }
+
+    function executeTransaction(uint _txIndex)
+        public
+        onlyOwner
+        txExists(_txIndex)
+        notExecuted(_txIndex)
+        {
+            Transaction storage transaction = transactions[_txIndex];
+
+            require(
+                transaction.numConfirmations >= numConfirmationsRequired,
+                "cannot execute tx"
+            );
+            transaction.executed = true;
+            (bool success, ) = transaction.to.call{value: transaction.value}(
+                transaction.data
+            );
+            require(success, "tx failed");
+
+            emit ExecuteTransaction(msg.sender, _txIndex);
+        }
+
+    function revokeConfirmation(uint _txIndex)
+        public
+        onlyOwner
+        txExists(_txIndex)
+        notExecuted(_txIndex)
+        {
+            Transaction storage transaction = transactions[_txIndex];
+
+            require(isConfirmed[_txIndex][msg.sender], "tx not confirmed");
+
+            transaction.numConfirmations -= 1;
+            isConfirmed[_txIndex][msg.sender] = false;
+
+            emit RevokeConfirmation(msg.sender, _txIndex);
+        }
+
+    function getOwners() public view returns (address[] memory) {
+        return owners;
+    }
+
+    function getTransactionCount() public view returns (uint) {
+        return transactions.length;
+    }
+
+    function getTransaction(uint _txIndex)
+        public
+        view
+        returns (
+            address to,
+            uint value,
+            bytes memory data,
+            bool executed,
+            uint numConfirmations
+        )
+        {
+            Transaction storage transaction = transactions[_txIndex];
+
+            return (
+                transaction.to,
+                transaction.value,
+                transaction.data,
+                transaction.executed,
+                transaction.numConfirmations
+            );
+        }
+}
+
+// here's a contract to test sending transactions from the multi-sig wallet
+contract TestContract {
+    uint public i;
+
+    function callMe(uint j) public {
+        i += j;
+    }
+
+    function getData() public pure returns (bytes memory) {
+        return abi.encodeWithSignature("callMe(uint256)", 123);
+    }
+}
+
+/*
+Merkle Tree
+merkle tree allows you to cryptographically prove that an element is contained in a set
+without revealing the entire set.
+*/
+
+contract MerkleProof {
+    function verify(
+        // The type byte[] is an array of bytes, but due to padding rules, it wastes 31 bytes of space for each element (except in storage). It is better to use the bytes type instead.
+        bytes32[] memory proof,
+        bytes32 root,
+        bytes32 leaf,
+        uint index
+    ) public pure returns (bool) {
+        bytes32 hash = leaf;
+
+        for (uint i = 0; i < proof.length; i++) {
+            bytes32 proofElement = proof[i];
+
+            if (index % 2 == 0) {
+                hash = keccak256(abi.encodePacked(hash, proofElement));
+            } else {
+                hash = keccak256(abi.encodePacked(proofElement, hash));
+            }
+
+            index = index / 2;
+        }
+
+        return hash == root;
+    }
+}
+
+contract TestMerkleProof is MerkleProof {
+    bytes32[] public hashes;
+
+    constructor() {
+        string[4] memory transactions = [
+            "alice -> bob",
+            "bob -> dave",
+            "carlo -> alice",
+            "dave -> bob"
+        ];
+
+        for (uint i = 0; i < transactions.length; i++) {
+            hashes.push(keccak256(abi.encodePacked(transactions[i])));
+        }
+
+        uint n = transactions.length;
+        uint offset = 0;
+
+        while (n > 0) {
+            for (uint i = 0; i < n - 1; i += 2){
+                hashes.push(
+                    keccak256(
+                        abi.encodePacked(hashes[offset + i], hashes[offset + i + 1])
+                    )
+                );
+            }
+            offset += n;
+            n = n / 2;
+        }
+    }
+
+    function getRoot() public view returns (bytes32) {
+        return hashes[hashes.length - 1];
+    }
+
+    /* verify
+    3rd leaf
+    0x1bbd78ae6188015c4a6772eb1526292b5985fc3272ead4c65002240fb9ae5d13
+
+    root
+    0x074b43252ffb4a469154df5fb7fe4ecce30953ba8b7095fe1e006185f017ad10
+
+    index
+    2
+
+    proof
+    0x948f90037b4ea787c14540d9feb1034d4a5bc251b9b5f8e57d81e4b470027af8
+    0x63ac1b92046d474f84be3aa0ee04ffe5600862228c81803cce07ac40484aee43
+    */
+}
+
+/*
+Iterable Mapping
+you cannot iterate through a mapping. So here is an example of how to create an iterable mapping.
+*/
+
+library IterableMapping {
+    // Iterable mapping from address to uint;
+    struct Map {
+        address[] keys;
+        mapping(address => uint) values;
+        mapping(address => uint) indexOf;
+        mapping(address => bool) inserted;
+    }
+
+    function get(Map storage map, address key) public view returns (uint) {
+        return map.values[key];
+    }
+
+    function getKeyAtIndex(Map storage map, uint index) public view returns (address) {
+        return map.keys[index];
+    }
+
+    function size(Map storage map) public view returns (uint) {
+        return map.keys.length;
+    }
+
+    function set(
+        Map storage map,
+        address key,
+        uint val
+    ) public {
+        if (map.inserted[key]) {
+            map.values[key] = val;
+        } else {
+            map.inserted[key] = true;
+            map.values[key] = val;
+            map.indexOf[key] = map.keys.length;
+            map.keys.push(key);
+        }
+    }
+
+    function remove(Map storage map, address key) public {
+        if (!map.inserted[key]) {
+            return;
+        }
+
+        delete map.inserted[key];
+        delete map.values[key];
+
+        uint index = map.indexOf[key];
+        uint lastIndex = map.keys.length - 1;
+        address lastKey = map.keys[lastIndex];
+
+        map.indexOf[lastKey] = index;
+        delete map.indexOf[key];
+
+        map.keys[index] = lastKey;
+        map.keys.pop();
+    }
+}
+
+contract TestIterableMap {
+    using IterableMapping for IterableMapping.Map;
+
+    IterableMapping.Map private map;
+
+    function testIterableMap() public {
+        map.set(address(0), 0);
+        map.set(address(1), 100);
+        map.set(address(2), 200); // insert
+        map.set(address(2), 200); // update
+        map.set(address(3), 300);
+
+        for (uint i = 0; i < map.size(); i++) {
+            address key = map.getKeyAtIndex(i);
+
+            assert(map.get(key) == i * 100);
+        }
+
+        map.remove(address(1));
+
+        // keys = [address(0), address(3), address(2)]
+        assert(map.size() == 3);
+        assert(map.getKeyAtIndex(0) == address(0));
+        assert(map.getKeyAtIndex(1) == address(3));
+        assert(map.getKeyAtIndex(2) == address(2));
+    }
+}
