3d973b2f7e97b25b4a64bfa4766d5b72680658f9
jasonshi10 <jasonxinshi@pm.me>
2022-01-18 20:21:51 -0800

    update function

--- simple_bank.sol
+++ simple_bank.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.6.6;
-// a simple Bank contract
+// a simple bank contract
 // allows deposits, withdrawals, and balance checks
 contract SimpleBank {
     // declare state variables outside function. persist through life of contract
@@ -25,5 +25,48 @@ contract SimpleBank {
         owner = msg.sender;
     }
 
+    /// @notice deposit ether into bank
+    /// @return the balance of the user after the deposit is made
+    function deposit() public payable returns (uint) {
+        // use 'require' to test user inputs, 'assert' for internal invariants
+        // here we are making sure that there isn't an overflow issue
+        require((balances[msg.sender] + msg.value) >= balances[msg.sender]);
+
+        balances[msg.sender] += msg.value;
+        // no "this." or "self." required with state variable
+        // all values set to data type's initial value by default
+
+        emit LogDepositMade(msg.sender, msg.value); // fire event
+
+        return balances[msg.sender];
+    }
+
+    /// @notice withdraw ether from bank
+    /// @dev this does not return any excess ether sent to it
+    /// @param withdrawAmount amount you want to withdraw
+    /// @return remainingBal
+    function withdraw(uint withdrawAmount) public returns (uint remainingBal) {
+        require(withdrawAmount <= balances[msg.sender]);
+
+        // note the way we deduct the balance right away, before sending
+        // every .transfer/.send from this contract can call an external function
+        // this may allow the caller to request an amount greater
+        // than their balance using a recursive call
+        // aim to commit state before calling external functions, including .transfer/.send
+        balances[msg.sender] -= withdrawAmount;
+
+        // this automatically throws on a failure, which means the updated balance is reverted
+        msg.sender.transfer(withdrawAmount);
+
+        return balances[msg.sender];
+    }
+
+    /// @notice get balance
+    /// @return the balance of the user
+    // 'view' (ex: constant) prevents function from editing state variables
+    // allows function to run locally/off blockchain
+    function balance() view public returns (uint) {
+        return balances[msg.sender];
+    }
 
 }
