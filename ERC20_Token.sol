a587a32f0ef62016293aad12f52aeedafe794fa7
jasonshi10 <jasonxinshi@pm.me>
2022-01-27 10:51:02 -0800

    update erc20

--- ERC20_Token.sol
+++ ERC20_Token.sol
@@ -1,60 +1,79 @@
-//SPDX-License-Identifier:MIT
-pragma solidity ^0.8.6;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
 
-// create a ERC20 token that is allowed to be traded on DEXs like Uniswap
-contract Token {
-    //public means the variable can be read outside the smart contract
-    string public name;
-    string public symbol;
-    uint256 public decimals;
-    uint256 public totalSupply;
+interface IERC20 {
+    // total amount of the erc20 tokens
+    function totalSupply() external view returns (uint);
+    // returns the balance
+    function balanceOf(address account) external view returns (uint);
+    // holders can call transfer to transfer tokens
+    function transfer(address recipient, uint amount) external returns (bool);
+    // holders can allow others to send on their behalf
+    function allowance(address owner, address spender)
+        external
+        view
+        returns (uint);
 
-    //mapping keeps track of key (address) and value
-    mapping(address => uint256) public balanceOf;
-    mapping(address => mapping(address => uint256)) public allowance;
+    function approve(address spender, uint amount) external returns (bool);
 
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint amount
+    ) external returns (bool);
 
-    // constructor executes when smartcontract is created
-    constructor(string memory _name, string memory _symbol, uint _decimals, uint _totalSupply) {
-        name = _name;
-        symbol = _symbol;
-        decimals = _decimals;
-        totalSupply = _totalSupply;
-        //msg.sender corresponds who called the function. when we create the contract we will have all the tokens as the sender
+    event Transfer(address indexed from, address indexed to, uint amount);
+    event Approval(address indexed owner, address indexed spender, uint amount);
+}
+
+contract ERC20 is IERC20 {
+    uint public totalSupply = 1000;
+    // keep track of how much tokens each user has
+    mapping(address => uint) public balanceOf;
+    // keep track of approved spenders and amount spent
+    mapping(address => mapping(address => uint)) public allowance;
+    string public name = "DEVToken";
+    string public symbol = "DEV";
+    uint8 public decimals = 18;
+
+    constructor() {
         balanceOf[msg.sender] = totalSupply;
     }
 
-    // internal function transfer can only be called by this contract
-    function _transfer(address _from, address _to, uint256 _value) internal {
-        // Ensure sending is to valid address! 0x0 address cane be used to burn()
-        require(_to != address(0));
-        balanceOf[_from] = balanceOf[_from] - (_value);
-        balanceOf[_to] = balanceOf[_to] + (_value);
-        emit Transfer(_from, _to, _value);
-    }
-
-    function transfer(address _to, uint256 _value) external returns (bool success) {
-        require(balanceOf[msg.sender] >= _value);
-        _transfer(msg.sender, _to, _value);
+    function transfer(address recipient, uint amount) external returns (bool) {
+        balanceOf[msg.sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(msg.sender, recipient, amount);
         return true;
     }
 
-    // allow _spender to spend up to _value on your behalf
-    function approve(address _spender, uint256 _value) external returns (bool) {
-        require(_spender != address(0));
-        allowance[msg.sender][_spender] = _value;
-        emit Approval(msg.sender, _spender, _value);
+    function approve(address spender, uint amount) external returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
         return true;
     }
 
-    // allow _spender to spend up to _value on your behalf
-    function transferFrom(address _from, address _to, uint256 _value) external returns (bool) {
-        require(_value <= balanceOf[_from]);
-        require(_value <= allowance[_from][msg.sender]);
-        allowance[_from][msg.sender] = allowance[_from][msg.sender] - (_value);
-        _transfer(_from, _to, _value);
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint amount
+    ) external returns (bool) {
+        allowance[sender][msg.sender] -= amount;
+        balanceOf[sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(sender, recipient, amount);
         return true;
+    }
+
+    function mint(uint amount) external {
+        balanceOf[msg.sender] += amount;
+        totalSupply += amount;
+        emit Transfer(address(0), msg.sender, amount);
+    }
+
+    function burn(uint amount) external {
+        balanceOf[msg.sender] -= amount;
+        totalSupply -= amount;
+        emit Transfer(msg.sender, address(0), amount);
     }
 }
