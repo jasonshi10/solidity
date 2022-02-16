7da4772e7ec0bec44bc7b08b3718d054b327801c
jasonshi10 <jasonxinshi@pm.me>
2022-02-16 11:34:07 -0800

    add more fn to nftmarketplace.sol

--- NftMarketplace.sol
+++ NftMarketplace.sol
@@ -20,32 +20,39 @@ contract NFT is ERC721URIStorage {
     }
 
     function createToken(string memory tokenURI) public returns (uint) {
-        // create unique ID for tokens
+        /// @dev create unique ID for tokens
         _tokenIds.increment();
-        // new var setting new tokenID UID for tokens
+        /// @dev new var setting new tokenID UID for tokens
         uint256 newItemId = _tokenIds.current();
-        // mint the token
+        /// @dev mint the token
         _mint(msg.sender, newItemId);
-        // use new tokenID and passing in URI
+        /// @dev use new tokenID and passing in URI
         _setTokenURI(newItemId, tokenURI);
-        // allow us to have access to this token and our other contract so that transfer tokens btw parties
+        /// @dev allow us to have access to this token and our other contract so that transfer tokens btw parties
         setApprovalForAll(contractAddress, true);
-        // need tokenId to transfer etc.
+        /// @dev need tokenId to transfer etc.
         return newItemId;
     }
 }
 
 /*
-ReentrancyGuard allows us to add non-reentrance modifier that prevents re-entry attacks.
+ReentrancyGuard adds non-reentrance modifier that prevents re-entry attacks.
 when you call a contract from another you often open the contract up for recursive calls
-meaning that before that other function has resolved then maybe someone will call the function again
+meaning that before that other function has resolved, someone may call the function again
 to drain funds etc.
 */
 contract NFTMarket is ReentrancyGuard {
     using Counters for Counters.Counter;
     Counters.Counter private _itemIds;
-    // keep track of how many items sold. we can also get how many remaining to be sold
+    /// @ dev keep track of how many items sold. we can also get how many remaining to be sold
     Counters.Counter private _itemsSold;
+
+    address payable owner;
+    uint256 listingPrice = 10 ether;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
 
     struct MarketItem {
         uint itemId;
@@ -55,10 +62,10 @@ contract NFTMarket is ReentrancyGuard {
         address payable owner;
         uint256 price;
     }
-    // this mapping allows us to retrive metadata of the item based on itemId
+    /// @dev this mapping allows us to retrive metadata of the item based on itemId
     mapping(uint256 => MarketItem) private idToMarketItem;
 
-    // emit an event when someone purchases a nft
+    /// @dev emit an event when someone purchases a nft
     event MarketItemCreated (
         uint indexed itemId,
         address indexed nftContract,
@@ -67,17 +74,18 @@ contract NFTMarket is ReentrancyGuard {
         address owner,
         uint256 price
     );
-    // once create the item we want to keep track of the id and price
+    /// @dev once create the item we want to keep track of the id and price
     function createMarketItem(
         address nftContract,
         uint256 tokenId,
         uint256 price
     ) public payable nonReentrant {
       require(price > 0, "Price must be at least 1 wei");
+      require(msg.value == listingPrice, "Price must be equal to listing price");
 
       _itemIds.increment();
       uint256 itemId = _itemIds.current();
-    // store the metadata into the mapping
+    /// @dev store the metadata into the mapping
       idToMarketItem[itemId] = MarketItem(
           itemId,
           nftContract,
@@ -105,9 +113,62 @@ contract NFTMarket is ReentrancyGuard {
     ) public payable nonReentrant {
         uint price = idToMarketItem[itemId].price;
         uint tokenId =idToMarketItem[itemId].tokenId;
-        // @dev have to submit a value of the price
+
         require(msg.value == price, "Please submit the asking price in order to complete the purchase");
 
+        idToMarketItem[itemId].seller.transfer(msg.value);
+
+        IERC721(nftContract).transferFrom(address(this), msg.sender, tokenId);
+        /// @dev set the owner to msg.sender after sale
+        idToMarketItem[itemId].owner = payable(msg.sender);
+        /// @dev keep track of the number of items sold
+        _itemsSold.increment();
+        /// @dev transfer the amount of listing price to the owner when sold
+        payable(owner).transfer(listingPrice);
+    }
+
+    /// @dev a function to return the items for sale, exclude items sold
+    function fetchMarketItems() public view returns (MarketItem[] memory) {
+        /// @dev total number of items created so far
+        uint itemCount = _itemIds.current();
+        /// @dev unsold number of items
+        uint unsoldItemCount = itemCount - _itemsSold.current();
+        uint currentIndex = 0;
+
+        MarketItem[] memory items = new MarketItem[](unsoldItemCount);
+        for (uint i = 0; i < itemCount; i++) {
+            if (idToMarketItem[i + 1].owner == address(0)) {
+                uint currentId = idToMarketItem[i + 1].itemId;
+                MarketItem storage currentItem = idToMarketItem[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+        return items;
+    }
+
+    /// @dev a function to return my NFT collections
+    function fetchMyNFTs() public view returns (MarketItem[] memory) {
+        uint totalItemCount = _itemIds.current();
+        uint itemCount = 0;
+        uint currentIndex = 0;
+
+        for (uint i = 0; i <= totalItemCount; i++) {
+            if (idToMarketItem[i + 1].owner == msg.sender) {
+                itemCount += 1;
+            }
+        }
+
+        MarketItem[] memory items = new MarketItem[](itemCount);
+        for (uint i = 0; i < totalItemCount; i++) {
+            if (idToMarketItem[i +1].owner == msg.sender) {
+                uint currentId = idToMarketItem[i + 1].itemId;
+                MarketItem storage currentItem = idToMarketItem[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+        return items;
     }
 
 }
--- solidity_by_example_challenges.sol
+++ solidity_by_example_challenges.sol
@@ -656,3 +656,32 @@ contract DataLocations {
         return myStructs[_addr].text;
     }
 }
+
+
+contract TodoList {
+    struct Todo {
+        string text;
+        bool completed;
+    }
+
+    Todo[] public todos;
+
+    function create(string calldata _text) external {
+        todos.push(Todo({text: _text, completed: false}));
+    }
+
+    function updateText(uint _index, string calldata _text) external {
+        todos[_index].text = _text;
+    }
+
+    function toggleCompleted(uint _index) external {
+        todos[_index].completed = !todos[_index].completed;
+    }
+
+    function get(uint _index) external view returns(string memory, bool) {
+        Todo storage todo = todos[_index];
+        return (todo.text, todo.completed);
+    }
+
+
+}
