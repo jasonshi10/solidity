dd51124131f3eb1146dd115bb4f3a1b2d2551c1c
jasonshi10 <jasonxinshi@pm.me>
2022-03-08 13:18:06 -0800

    add more fn

--- solidity_by_example_challenges.sol
+++ solidity_by_example_challenges.sol
@@ -1036,3 +1036,41 @@ contract Payable {
     // be able to sesnd eth when call this function
     function deposit() payable external {}
 }
+
+/*
+fallback is a function that is called when a function to call does not exist.
+
+For example, call doesNotExist(), this will trigger the fallback function.
+Receive Ether
+
+fallback function declared as external payable is commonly used to enable the contract to receive Ether.
+
+There is a slight variation of the fallback called receive.
+
+receive() external payable is called if msg.data is empty.
+Which is called, fallback or receive?
+
+Here is a graph summarizing which function is called:
+
+Which function is called, fallback() or receive()?
+
+    Ether is sent to contract
+               |
+        is msg.data empty?
+              / \\
+            yes  no
+            /     \\
+receive() exists?  fallback()
+         /   \\
+        yes   no
+        /      \\
+    receive()   fallback()
+*/
+
+contract Fallback {
+    string[] public answers = ["receive", "fallback"];
+
+    fallback() external payable {}
+
+    receive() external payable {}
+}
