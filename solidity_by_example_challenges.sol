700b1de83d59a0d91ccdad127691bff236116c43
jasonshi10 <jasonxinshi@pm.me>
2022-03-14 21:21:27 -0700

    add contract calling eg

--- solidity_by_example_challenges.sol
+++ solidity_by_example_challenges.sol
@@ -1113,3 +1113,42 @@ contract SendEther {
         require(sent, "failed to send ether");
     }
 }
+
+/*
+A Contract Calling Another Contract
+*/
+
+import "./TestContract.sol";
+
+contract CallTestContract {
+    function setX(TestContract _test, uint _x) external {
+        _test.setX(_x);
+    }
+
+    function setXfromAddress(address _addr, uint _x) external {
+        TestContract test = TestContract(_addr);
+        test.setX(_x);
+    }
+
+    function getX(address _addr) external view returns (uint) {
+        uint x = TestContract(_addr).getX();
+        return x;
+    }
+
+    function setXandSendEther(TestContract _test, uint _x) external payable {
+        _test.setXandReceiveEther{value: msg.value}(_x);
+    }
+
+    function getXandValue(address _addr) external view returns (uint, uint) {
+        (uint x, uint value) = TestContract(_addr).getXandValue();
+        return (x, value);
+    }
+
+    function setXwithEther(address _addr) external payable {
+        TestContract(_addr).setXtoValue{ value: msg.value }();
+    }
+
+    function getValue(address _addr) external view returns (uint) {
+        return TestContract(_addr).getValue();
+    }
+}
