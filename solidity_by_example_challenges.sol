327bd1d0cf6a6fe3f0d83e8e336992273a889a0e
jasonshi10 <jasonxinshi@pm.me>
2022-03-16 20:36:57 -0700

    add interface

--- solidity_by_example_challenges.sol
+++ solidity_by_example_challenges.sol
@@ -1152,3 +1152,28 @@ contract CallTestContract {
         return TestContract(_addr).getValue();
     }
 }
+
+/*
+Interfaces enable a contract to call other contracts without having its code.
+You know what functions you can call, so you define an interface to TestInterface.
+*/
+
+interface ITestInterface {
+    function count() external view returns (uint);
+
+    function inc() external;
+
+    function dec() external;
+}
+
+// Contract that uses TestInterface interface to call TestInterface contract
+contract CallInterface {
+    function examples(address _test) external {
+        ITestInterface(_test).inc();
+        uint count = ITestInterface(_test).count();
+    }
+
+    function dec(address _test) external {
+        ITestInterface(_test).dec();
+    }
+}
