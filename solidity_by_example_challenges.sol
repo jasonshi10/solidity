ccd8419f3b0085659220cf07c5dba725de3fe1db
jasonshi10 <jasonxinshi@pm.me>
2022-03-18 13:15:31 -0700

    add delegatecall

--- solidity_by_example_challenges.sol
+++ solidity_by_example_challenges.sol
@@ -1208,3 +1208,48 @@ contract Call {
         require(success, "tx failed");
     }
 }
+
+/*
+delegatecall is like call, except the code of callee is executed inside the caller.
+
+For example contract A calls delegatecall on contract B. Code inside B is executed using A's context such as storage, msg.sender and msg.value.
+*/
+
+// This is the contract that is called:
+contract TestDelegateCall {
+    // Storage layout must be the same as contract A
+    uint public num;
+    address public sender;
+    uint public value;
+
+    function setVars(uint _num) external payable {
+        num = _num;
+        sender = msg.sender;
+        value = msg.value;
+    }
+
+    function setNum(uint _num) external {
+        num = _num;
+    }
+}
+// This is the delegatecall:
+contract DelegateCall {
+    uint public num;
+    address public sender;
+    uint public value;
+
+    function setVars(address _test, uint _num) external payable {
+        // This contract's storage is updated, TestDelegateCall's storage is not modified.
+        (bool success, bytes memory data) = _test.delegatecall(
+            abi.encodeWithSignature("setVars(uint256)", _num)
+        );
+        require(success, "tx failed");
+    }
+
+    function setNum(address _test, uint _num) external {
+        (bool success, bytes memory data) = _test.delegatecall(
+            abi.encodeWithSignature("setNum(uint256)", _num)
+        );
+        require(success, "tx failed");
+    }
+}
