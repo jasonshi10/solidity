937727bf324390ad3144471ab186c6a74892403b
jasonshi10 <jasonxinshi@pm.me>
2022-03-17 18:29:27 -0700

    add call contract

--- solidity_by_example_challenges.sol
+++ solidity_by_example_challenges.sol
@@ -1177,3 +1177,34 @@ contract CallInterface {
         ITestInterface(_test).dec();
     }
 }
+
+/*
+
+*/
+contract Call {
+    function callFoo(address payable _addr) external payable {
+        // You can send ether and specify a custom gas amount
+        // returns 2 outputs (bool, bytes)
+        // bool - whether function executed successfully or not (there was an error)
+        // bytes - any output returned from calling the function
+        (bool success, bytes memory data) = _addr.call{
+            value: msg.value,
+            gas: 5000
+        }(abi.encodeWithSignature("foo(string,uint256)", "call foo", 123));
+        require(success, "tx failed");
+    }
+
+    // Calling a function that does not exist triggers the fallback function, if it exists.
+    function callDoesNotExist(address _addr) external {
+        (bool success, bytes memory data) = _addr.call(
+            abi.encodeWithSignature("doesNotExist()")
+        );
+    }
+
+    function callBar(address _addr) external {
+        (bool success,) = _addr.call(
+            abi.encodeWithSignature("bar(uint256,bool)", 123, true)
+            );
+        require(success, "tx failed");
+    }
+}
