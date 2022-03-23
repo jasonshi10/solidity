325fd1411672d32ad39335c9143df7996f97e45e
jasonshi10 <jasonxinshi@pm.me>
2022-03-22 21:09:13 -0700

    add New fn

--- solidity_by_example_challenges.sol
+++ solidity_by_example_challenges.sol
@@ -1253,3 +1253,44 @@ contract DelegateCall {
         require(success, "tx failed");
     }
 }
+
+/*
+New
+New contracts can be created from a contract using the keyword new.
+This is the contract that is deployed using new for this challenge.
+
+pragma solidity ^0.8.10;
+
+contract Account {
+    address public bank;
+    address public owner;
+    uint public withdrawLimit;
+
+    constructor(address _owner, uint _withdrawLimit) payable {
+        bank = msg.sender;
+        owner = _owner;
+        withdrawLimit = _withdrawLimit;
+    }
+}
+*/
+
+import "./Account.sol";
+
+contract Bank {
+    Account[] public accounts;
+
+    function createAccount(address _owner) external {
+        Account account = new Account(_owner, 0);
+        accounts.push(account);
+    }
+
+    function createAccountAndSendEther(address _owner) external payable {
+        Account account = (new Account){value: msg.value}(_owner, 0);
+        accounts.push(account);
+    }
+
+    function createSavingsAccount(address _owner) external {
+        Account account = new Account(_owner, 1000);
+        accounts.push(account);
+    }
+}
