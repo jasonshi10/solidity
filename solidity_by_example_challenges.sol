32514b0c12a0f06f8b2dd4301f98a7231df85aab
jasonshi10 <jasonxinshi@pm.me>
2022-03-23 12:52:56 -0700

    add library

--- solidity_by_example_challenges.sol
+++ solidity_by_example_challenges.sol
@@ -1294,3 +1294,62 @@ contract Bank {
         accounts.push(account);
     }
 }
+
+/*
+Library
+Libraries are similar to contracts, but you can't declare any state variable and you can't send Ether.
+A library is embedded into the contract if all functions in the library are internal.
+Otherwise the library must be deployed and then linked before the contract is deployed.
+*/
+library Math {
+    function max(uint x, uint y) internal pure returns (uint) {
+        return x >= y ? x : y;
+    }
+
+    function min(uint x, uint y) internal pure returns (uint) {
+        return x < y ? x : y;
+    }
+}
+
+contract TestMath {
+    function testMax(uint x, uint y) external pure returns (uint) {
+        return Math.max(x, y);
+    }
+
+    function testMin(uint x, uint y) external pure returns (uint) {
+        return Math.min(x, y);
+    }
+}
+
+library ArrayLib {
+    function find(uint[] storage arr, uint x) internal view returns (uint) {
+        for (uint i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                return i;
+            }
+        }
+        revert("not found");
+    }
+
+    function sum(uint[] storage arr) internal view returns (uint) {
+        uint s;
+        for (uint i = 0; i < arr.length; i++) {
+            s += arr[i];
+        }
+        return s;
+    }
+}
+
+contract TestArray {
+    using ArrayLib for uint[];
+
+    uint[] public arr = [3, 2, 1];
+
+    function testFind() external view {
+        arr.find(2);
+    }
+
+    function testSum() external view returns (uint) {
+        return arr.sum();
+    }
+}
