8480726ac03cfd2fb3f148c92675e3f19bec550a
jasonshi10 <jasonxinshi@pm.me>
2022-03-24 17:22:14 -0700

    add keccak256

--- solidity_by_example_challenges.sol
+++ solidity_by_example_challenges.sol
@@ -1353,3 +1353,26 @@ contract TestArray {
         return arr.sum();
     }
 }
+
+/*
+keccak256 is a cryptographic hash funciton commonly used in Solidity.
+
+Some use cases are:
+
+    Creating a deterministic unique ID from inputs
+    Cryptographic signatures
+    Commit reveal scheme
+*/
+contract HashFunc {
+    function hash(
+        string memory _text,
+        uint _num,
+        address _addr
+    ) external pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_text, _num, _addr));
+    }
+
+    function getHash(address _addr, uint _num) external pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_addr, _num));
+    }
+}
