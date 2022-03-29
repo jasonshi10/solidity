50ef576e9440ab94b32ebd67f7d1c861d19a08a1
jasonshi10 <jasonxinshi@pm.me>
2022-03-28 20:14:49 -0700

    add access control

--- solidity_by_example_challenges.sol
+++ solidity_by_example_challenges.sol
@@ -1467,3 +1467,42 @@ contract VerifySig {
         signed = true;
     }
 }
+
+/*
+Access Control
+Implement role based access control
+*/
+contract AccessControl {
+    event GrantRole(bytes32 indexed role, address indexed account);
+    event RevokeRole(bytes32 indexed role, address indexed account);
+
+    mapping(bytes32 => mapping(address => bool)) public roles;
+
+    bytes32 public constant ADMIN = keccak256(abi.encodePacked("ADMIN"));
+    bytes32 public constant USER = keccak256(abi.encodePacked("USER"));
+
+    constructor() {
+        _grantRole(ADMIN, msg.sender);
+    }
+
+    modifier onlyRole(bytes32 _role) {
+        require(roles[_role][msg.sender], 'not an admin');
+        _;
+    }
+
+    function _grantRole(bytes32 _role, address _account) internal {
+        roles[_role][_account] = true;
+        emit GrantRole(_role, _account);
+    }
+
+    function grantRole(bytes32 _role, address _account) external onlyRole(ADMIN) {
+        _grantRole(_role, _account);
+
+    }
+
+    function revokeRole(bytes32 _role, address _account) external onlyRole(ADMIN) {
+        roles[_role][_account] = false;
+        emit RevokeRole(_role, _account);
+
+    }
+}
