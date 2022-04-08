228df627901c6508eca83056a849b04e75c88eb3
jasonshi10 <jasonxinshi@pm.me>
2022-04-07 19:10:14 -0700

    add delete contract

--- solidity_by_example_challenges.sol
+++ solidity_by_example_challenges.sol
@@ -1506,3 +1506,23 @@ contract AccessControl {
 
     }
 }
+
+/*
+Deleting contracts
+Contracts can be deleted forever from the blockchain with the function selfdestruct.
+
+selfdestruct takes in a single argument, a payable address to forcefully send all of Ether store in the contract.
+
+For example:
+
+  selfdestruct(payable(msg.sender))
+
+Casts msg.sender to a payable address
+Sends all of Ether held by the contract to msg.sender
+Deletes the contract from the blockchain.
+*/
+contract Kill {
+    function kill() external {
+        selfdestruct(payable(msg.sender));
+    }
+}
